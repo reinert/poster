a7f5f8dff96a68d9b071ff8b3fff28fef8c6bc30
Danilo Reinert <daniloreinert@gmail.com>
2022-04-04 19:15:07 -0300

    Post resource

---
+++ server/src/resource/post_resource.js
@@ -0,0 +1,31 @@
+import base from "./base_resource.js";
+
+export default (service) => [
+    {
+        method: 'POST',
+        url: '/posts',
+        handler: async (req) => {
+            return service.addPost(req.body)
+        }
+    },
+    {
+        method: 'GET',
+        url: '/posts',
+        handler: async (req) => {
+            if (req.query.user_id)
+                return service.getUserPosts(req.query.user_id, req.query.limit, req.query.offset)
+
+            if (req.query.follower_id)
+                return service.getFollowingPosts(req.query.follower_id, req.query.limit, req.query.offset)
+
+            return service.getPosts(req.query.limit, req.query.offset)
+        }
+    },
+    {
+        method: 'GET',
+        url: '/posts/:post_id',
+        handler: async (req) => {
+            return base.notFoundIfEmpty(await service.getPost(req.params.post_id))
+        }
+    }
+]
