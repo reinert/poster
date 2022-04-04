820a2cf070c8c830d48ef010b9a176793633868c
Danilo Reinert <daniloreinert@gmail.com>
2022-04-04 19:07:02 -0300

    Abstract post repository

---
+++ server/src/repository/post_repository.js
@@ -0,0 +1,26 @@
+export default class PostRepository {
+
+    async addPost(post) {
+        throw new Error('Not implemented yet.')
+    }
+
+    async getPost(post_id) {
+        throw new Error('Not implemented yet.')
+    }
+
+    async getPosts(limit = 10, offset = 0) {
+        throw new Error('Not implemented yet.')
+    }
+
+    async getPostsByFollower(follower_id, limit = 10, offset = 0) {
+        throw new Error('Not implemented yet.')
+    }
+
+    async getPostsByUser(user_id, limit = 10, offset = 0) {
+        throw new Error('Not implemented yet.')
+    }
+
+    async getPostsByUserAndDate(user_id, date) {
+        throw new Error('Not implemented yet.')
+    }
+}
