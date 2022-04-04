f845437642090b1eb8164c818306f27d13dc43a6
Danilo Reinert <daniloreinert@gmail.com>
2022-04-04 19:01:18 -0300

    Users migration

---
+++ pgsql/migrations/0001_-_users.sql
@@ -0,0 +1,16 @@
+CREATE TABLE users
+(
+  id bigint GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  username varchar(14) NOT NULL,
+  join_date timestamptz NOT NULL DEFAULT now(),
+
+  -- Computed columns that will be updated according to new follows/unfollows
+  nr_followers int NOT NULL DEFAULT 0,
+  nr_following int NOT NULL DEFAULT 0,
+
+  -- Check username is alphanumeric
+  CONSTRAINT users_username_ck CHECK (username ~ '^[[:alnum:]]+$'),
+
+  -- Check username is unique
+  CONSTRAINT users_username_uq UNIQUE (username)
+);
