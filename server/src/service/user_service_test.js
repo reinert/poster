1160c2f91a48af31bd85950536ce3c8cc96dd7cb
Danilo Reinert <daniloreinert@gmail.com>
2022-04-04 23:45:06 -0300

    User service tests

---
+++ server/src/service/user_service_test.js
@@ -0,0 +1,29 @@
+import test from 'ava'
+import base from '../test_base.js'
+import fake_data from "../fake_data.js";
+import PgUserRepository from "../pg_repository/user_repository.js";
+import UserService from "./user_service.js";
+
+const { before, after } = base
+
+const { PostKind, generatePost, generateUsers } = fake_data
+
+const TEST_DB = 'user_service_test'
+
+before(test, TEST_DB, async t => {
+    t.context.service = new UserService(new PgUserRepository(t.context.pool))
+    // const [ user ] = await generateUsers(t.context.pool, 1)
+    // t.context.user = user
+})
+
+after(test, TEST_DB)
+
+test('addUserFailsWhenUsernameIsNonAlphanumeric', async t => {
+    const username = '!@#!@%$!#$!'
+    await t.throwsAsync(t.context.service.addUser(username))
+})
+
+test('followFailsWhenFollowerAndFolloweeAreEqual', async t => {
+    const [ user ] = await t.context.service.addUser({ username: 'testuser' })
+    await t.throwsAsync(t.context.service.follow(user.id, user.id))
+})
