5aaf316476ec8ad5a69165a1970437d5245ca1db
Danilo Reinert <daniloreinert@gmail.com>
2022-04-08 10:33:19 -0300

    Add in_transaction to PgRepository

--- server/src/pg_repository/pg_repository.js
+++ server/src/pg_repository/pg_repository.js
@@ -6,10 +6,32 @@ const PgRepository = (Repository) => class extends Repository {
     }
 
     async query({ name, text, values}) {
+        // note: we don't try/catch this because if connecting throws an exception
+        // we don't need to dispose of the client (it will be undefined)
         const client = await this.pool.connect()
         try {
             const r = await client.query({name, text, values})
             return r.rows
+        } finally {
+            client.release()
+        }
+    }
+
+    async in_transaction(callback) {
+        // note: we don't try/catch this because if connecting throws an exception
+        // we don't need to dispose of the client (it will be undefined)
+        const client = await this.pool.connect()
+        try {
+            await client.query('BEGIN')
+
+            const result = await callback(client)
+
+            await client.query('COMMIT')
+
+            return result
+        } catch (e) {
+            await client.query('ROLLBACK')
+            throw e
         } finally {
             client.release()
         }
